70c8901c62fef750db275b33334560f1f34a9762
anshojha <anshojha24@gmail.com>
2023-01-10 23:15:20 +0530

    Learn about the opertator

---
+++ calc.py
@@ -0,0 +1,29 @@
+# code to write a file
+
+import os
+import sys
+import time
+import pyautogui as pt
+import random
+
+# Path to the file
+
+
+# Path to the file
+path = "C:\\Users\\HP\\Desktop\\Python\\spam.py"
+
+# Open the file
+file = open(path, "r")
+
+# Read the file
+code = file.read()
+
+# Close the file
+file.close()
+
+# Path to the file
+
+
+# Path to the file
+
+
---
+++ reading_a_dir.py
@@ -0,0 +1,6 @@
+import os
+print(os.listdir())
+
+
+
+'''It help us to print the all the files and content of the current folder'''
